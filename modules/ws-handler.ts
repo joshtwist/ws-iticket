16969b961cb50454507898aab794c1413920e1a2
joshtwist <josh@zuplo.com>
2023-06-12 22:59:50 +0000

    Initial commit from Zuplo connect

---
+++ modules/ws-handler.ts
@@ -0,0 +1,33 @@
+import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
+
+type MyPolicyOptionsType = {
+  myOption: any;
+};
+export default async function (
+  request: ZuploRequest,
+  context: ZuploContext,
+  options: MyPolicyOptionsType,
+  policyName: string
+) {
+  // your policy code goes here, and can use the options to perform any
+  // configuration
+  // See the docs: https://www.zuplo.com/docs/policies/custom-code-inbound
+
+  const upgradeHeader = request.headers.get('Upgrade');
+  if (!upgradeHeader || upgradeHeader !== 'websocket') {
+    return new Response('Expected Upgrade: websocket', { status: 426 });
+  }
+
+  const webSocketPair = new WebSocketPair();
+  const [client, server] = webSocketPair;
+
+  server.accept();
+  server.addEventListener('message', event => {
+    context.log.error(event.data);
+  });
+
+  return new Response(null, {
+    status: 101,
+    webSocket: client,
+  });
+}
