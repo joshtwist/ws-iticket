13384e4cbf87987bef69de829570871ecf2e742d
joshtwist <josh@zuplo.com>
2023-06-12 23:18:43 +0000

    try/catch

--- modules/ws-handler.ts
+++ modules/ws-handler.ts
@@ -11,25 +11,30 @@ export default async function (
 ) {
   context.log.error(`request received: ${request.method} - '${request.url}'`);
 
-  // your policy code goes here, and can use the options to perform any
-  // configuration
-  // See the docs: https://www.zuplo.com/docs/policies/custom-code-inbound
+  try {
+    // your policy code goes here, and can use the options to perform any
+    // configuration
+    // See the docs: https://www.zuplo.com/docs/policies/custom-code-inbound
 
-  const upgradeHeader = request.headers.get('Upgrade');
-  if (!upgradeHeader || upgradeHeader !== 'websocket') {
-    return new Response('Expected Upgrade: websocket', { status: 426 });
+    const upgradeHeader = request.headers.get('Upgrade');
+    if (!upgradeHeader || upgradeHeader !== 'websocket') {
+      return new Response('Expected Upgrade: websocket', { status: 426 });
+    }
+
+    const webSocketPair = new WebSocketPair();
+    const [client, server] = Object.values(webSocketPair);
+
+    server.accept();
+    server.addEventListener('message', event => {
+      context.log.error(event.data);
+    });
+
+    return new Response(null, {
+      status: 101,
+      webSocket: client,
+    });
   }
-
-  const webSocketPair = new WebSocketPair();
-  const [client, server] = Object.values(webSocketPair);
-
-  server.accept();
-  server.addEventListener('message', event => {
-    context.log.error(event.data);
-  });
-
-  return new Response(null, {
-    status: 101,
-    webSocket: client,
-  });
+  catch (err) {
+    context.log.error('error', err);
+  }
 }
