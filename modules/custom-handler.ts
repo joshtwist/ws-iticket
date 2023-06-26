06a56b864a97c0195eba37256b661ec0dd9a9b19
joshtwist <josh@zuplo.com>
2023-06-26 18:10:39 +0000

    error

--- modules/custom-handler.ts
+++ modules/custom-handler.ts
@@ -3,10 +3,16 @@ import { SocketHandler } from "./socket-handler";
 const sh: SocketHandler = {
   onOpen: async (source, context) => {
     source.send('OK');
-    context.log.info('open');
+    context.log.error('open');
   },
   onMessage: async (message, source, context) => {
-    context.log.info(`message recevied: '${message}'`);
+    context.log.error(`message recevied: '${message}'`);
+  },
+  onClose: async (data, source, context) => {
+    context.log.error(`close`, data);
+  },
+  onError: async (error, source, context) => {
+    context.log.error('error', error);
   }
 }
 
